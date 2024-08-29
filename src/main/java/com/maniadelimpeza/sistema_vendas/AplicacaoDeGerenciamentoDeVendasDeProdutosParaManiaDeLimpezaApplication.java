c213102732bb1577cf516479410fa14c5623cd1f
Felipe Alves <felipealvess_hpg_@hotmail.com>
2024-08-28 22:15:54 -0300

    Criação do Projeto com SpringBoot

---
+++ src/main/java/com/maniadelimpeza/sistema_vendas/AplicacaoDeGerenciamentoDeVendasDeProdutosParaManiaDeLimpezaApplication.java
@@ -0,0 +1,13 @@
+package com.maniadelimpeza.sistema_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoDeGerenciamentoDeVendasDeProdutosParaManiaDeLimpezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoDeGerenciamentoDeVendasDeProdutosParaManiaDeLimpezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maniadelimpeza/sistema_vendas/AplicacaoDeGerenciamentoDeVendasDeProdutosParaManiaDeLimpezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maniadelimpeza.sistema_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoDeGerenciamentoDeVendasDeProdutosParaManiaDeLimpezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
